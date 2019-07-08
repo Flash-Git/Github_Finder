7602c057580e169bda8453c0f3a0d6518962642f
Flash <flash.jaq@gmail.com>
2019-07-08 03:57:50 +0200

    Added default props

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export class Navbar extends Component {
+  static defaultProps = {
+    title: "Github Finder",
+    icon: ["fab", "github"]
+  };
+
   render() {
     return(
       <nav className="navbar bg-primary">
         <h1>
-          <FontAwesomeIcon icon={["fab", "github"]} /> Navbar
+          <FontAwesomeIcon icon={ this.props.icon } /> { this.props.title }
         </h1>
       </nav>
     );
