f36d314a904aa7cc93e98755f2cb0ae326eebc26
Flash <flash.jaq@gmail.com>
2019-07-08 17:34:23 +0200

    Getting users from github api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from "axios";
 import './App.css';
 
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -10,12 +11,24 @@ import Users from './components/users/Users';
 library.add(faGithub);
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading:true });
+    const res = await axios.get("https://api.github.com/users");
+
+    this.setState({users: res.data, loading: false });
+  }
+
   render(){
     return(
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={ this.state.loading } users={ this.state.users } />
         </div>
       </div>
     );
