3af09cc7760dcba5122e0587f0fc4c3170eee015
Flash <flash.jaq@gmail.com>
2019-07-08 19:41:33 +0200

    Added condition to clear

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -7,7 +7,9 @@ class Search extends Component {
   };
 
   static propTypes = {
-    searchUsers: PropTypes.func.isRequired
+    showClear: PropTypes.bool.isRequired,
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired
   };
   
   onChange = e => {
@@ -37,9 +39,14 @@ class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
+        { this.props.showClear &&(
+          <button className="btn btn-light btn-block" onClick={ this.props.clearUsers }>
+            Clear
+          </button>
+        ) }
       </div>
     );
-  };
+  }
 }
 
 export default Search;
