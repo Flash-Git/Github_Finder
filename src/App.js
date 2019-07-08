ed387cdfd861d6c9a8056e34681681f13e811792
Flash <flash.jaq@gmail.com>
2019-07-08 19:01:08 +0200

    Added api key

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
 
   async componentDidMount() {
     this.setState({ loading:true });
-    const res = await axios.get("https://api.github.com/users");
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}£client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
     this.setState({users: res.data, loading: false });
   }
@@ -32,7 +32,7 @@ class App extends Component {
         </div>
       </div>
     );
-  }
+  };
 }
 
 export default App;
