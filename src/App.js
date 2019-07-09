e9bf6e5f5ec14051f2084900352d19a9ddfc907b
Flash <flash.jaq@gmail.com>
2019-07-09 16:26:33 +0200

    Added info to user page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { faInfoCircle, faCheckCircle, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
@@ -14,8 +14,7 @@ import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 
-library.add(faGithub);
-library.add(faInfoCircle);
+library.add(faGithub, faInfoCircle, faCheckCircle, faTimesCircle);
 
 class App extends Component {
   state = {
