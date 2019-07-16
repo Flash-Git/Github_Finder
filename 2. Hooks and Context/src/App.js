6fcf69d736e9056b855320a6e578dbb4941c6b99
Flash <flash.jaq@gmail.com>
2019-07-16 03:20:25 +0200

    Formatting

--- 2. Hooks and Context/src/App.js
+++ 2. Hooks and Context/src/App.js
@@ -1,38 +1,46 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faInfoCircle, faCheckCircle, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faInfoCircle,
+  faCheckCircle,
+  faTimesCircle
+} from "@fortawesome/free-solid-svg-icons";
 
-import Navbar from './components/layout/Navbar';
-import User from './components/users/User';
-import Alert from './components/layout/Alert';
+import Navbar from "./components/layout/Navbar";
+import User from "./components/users/User";
+import Alert from "./components/layout/Alert";
 import Home from "./components/pages/Home";
-import About from './components/pages/About';
-import NotFound from './components/pages/NotFound';
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
 
-import './App.css';
+import "./App.css";
 
 library.add(faGithub, faInfoCircle, faCheckCircle, faTimesCircle);
 
-const App = () => <GithubState> <AlertState>
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert />
-          <Switch>
-            <Route exact path="/" component={ Home } />
-            <Route exact path="/about" component={ About } />
-            <Route exact path="/user/:login" component={ User } />
-            <Route component={ NotFound } />
-          </Switch>
+const App = () => (
+  <GithubState>
+    <AlertState>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/user/:login" component={User} />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
         </div>
-      </div>
-    </Router>
-  </AlertState> </GithubState>;
+      </Router>
+    </AlertState>
+  </GithubState>
+);
 
 export default App;
